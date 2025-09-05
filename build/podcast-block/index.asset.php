ca76d4f1d38a3e6ee12703fefd09b0db1e216eee
Tom Finley <finley@bermanco.com>
2025-09-04 23:52:14 -0400

    refinements to backend control layout styling

--- build/podcast-block/index.asset.php
+++ build/podcast-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dfda00ebd90a530abb57');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6cfc91c0250520324665');
